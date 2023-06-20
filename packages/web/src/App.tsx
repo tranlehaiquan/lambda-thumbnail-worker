2122221f4d5b9b5df17a89793f6f26269ac88a85
quantran020173 <tranlehaiquan@gmail.com>
2023-06-21 00:25:53 +0700

    update

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,16 +1,39 @@
 import "./App.css";
+import { UploadOutlined } from "@ant-design/icons";
+import type { UploadProps } from "antd";
+import { Button, message, Upload } from "antd";
+
+const props: UploadProps = {
+  name: "file",
+  action: async () => {
+    const data = await fetch(import.meta.env.VITE_APP_API_URL, {
+      method: "POST",
+    });
+
+    const response = await data.json();
+
+    return response.preSigned;
+  },
+  method: "PUT",
+  multiple: false,
+  onChange(info) {
+    if (info.file.status !== "uploading") {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === "done") {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === "error") {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
 
 function App() {
-  function onClick() {
-    console.log("clicked");
-  }
-
   return (
     <div className="App">
-      <div className="card">
-        <input type="file" />
-        <button onClick={onClick}>Upload</button>
-      </div>
+      <Upload {...props}>
+        <Button icon={<UploadOutlined />}>Click to Upload</Button>
+      </Upload>
     </div>
   );
 }
